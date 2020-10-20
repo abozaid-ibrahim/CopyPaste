e8415d93a1dba97c6f88dd2d0c8b60507408e477
abuzeid <abuzeid7@gmail.com>
2020-10-20 17:32:16 +0200

    Add Reachability, Image downloader

---
+++ CopyPasteSwift/Components/ImageDownloader.swift
@@ -0,0 +1,65 @@
+//
+//  ImageDownloader.swift
+//  Marvel
+//
+//  Created by abuzeid on 23.09.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+typealias DownloadImageCompletion = (UIImage?) -> Void
+protocol ImageDownloaderType {
+    func downloadImageWith(url: URL, completion: DownloadImageCompletion?) -> Disposable?
+    func cached(url: String) -> UIImage?
+}
+
+public protocol Disposable {
+    func dispose()
+}
+
+extension URLSessionDataTask: Disposable {
+    public func dispose() {
+        cancel()
+    }
+}
+
+public final class ImageDownloader: ImageDownloaderType {
+    func downloadImageWith(url: URL, completion: DownloadImageCompletion? = nil) -> Disposable? {
+        if let data = cached(url: url.absoluteString) {
+            completion?(data)
+            return nil
+        }
+        let dataTask = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data,
+                let image = UIImage(data: data) else {
+                completion?(nil)
+                return
+            }
+            completion?(image)
+        }
+        dataTask.resume()
+        return dataTask
+    }
+}
+
+// MARK: Caching
+
+extension ImageDownloader {
+    func cached(url: String) -> UIImage? {
+        /// cach it yourself if you want
+        return nil
+    }
+}
+
+extension UIImageView {
+    @discardableResult
+    func setImage(with path: String) -> Disposable? {
+        guard let url = URL(string: path) else { return nil }
+        return ImageDownloader().downloadImageWith(url: url, completion: { [weak self] image in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        })
+    }
+}
---
+++ CopyPasteSwift/Components/Reachiblity.swift
@@ -0,0 +1,44 @@
+//
+//  Reachiblity.swift
+//  Marvel
+//
+//  Created by abuzeid on 28.09.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+
+import Foundation
+import Network
+
+protocol Reachable {
+    func hasInternet() -> Bool
+}
+
+/// you have to call Reachable.shared.startInternetTracking() at your app delegate
+
+final class Reachability: Reachable {
+    static let shared = Reachability()
+    private init() {}
+    let internetMonitor = NWPathMonitor()
+    let internetQueue = DispatchQueue(label: "InternetMonitor")
+    private var hasConnectionPath = false
+
+    func startInternetTracking() {
+        guard internetMonitor.pathUpdateHandler == nil else {
+            return
+        }
+        internetMonitor.pathUpdateHandler = { update in
+            if update.status == .satisfied {
+                self.hasConnectionPath = true
+            } else {
+                self.hasConnectionPath = false
+            }
+        }
+        internetMonitor.start(queue: internetQueue)
+    }
+
+    /// will tell you if the device has an Internet connection
+    /// - Returns: true if there is some kind of connection
+    func hasInternet() -> Bool {
+        return hasConnectionPath
+    }
+}
