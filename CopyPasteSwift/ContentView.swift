493d0873edcaa34d717a1fdc9c01d057c2d64a5a
abuzeid <abuzeid7@gmail.com>
2020-10-20 17:23:12 +0200

    Initial Commit

---
+++ CopyPasteSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CopyPasteSwift
+//
+//  Created by abuzeid on 20.10.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
