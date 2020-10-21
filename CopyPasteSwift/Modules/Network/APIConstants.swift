491bb5366b7a2fef71e207ff622c4df7f094e359
abuzeid <abuzeid7@gmail.com>
2020-10-22 01:22:07 +0200

    Add network module.

---
+++ CopyPasteSwift/Modules/Network/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//
+//  Created by abuzeid on 14.10.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+
+import Foundation
+struct APIConstants {
+    static let baseURL = "https://api.flickr.com/services/rest/"
+    static let apiKey = "e37adece6eb0fc58b8a560b2a433fa41"
+}
